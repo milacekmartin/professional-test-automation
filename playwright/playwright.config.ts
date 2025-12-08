b8cd9e3100b9cae250fbf5d3389585329a8fcc7d
Martin Milacek <milacema@gmail.com>
2025-12-08 20:18:25 +0100

    GH #09

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
                 detail: true,
                 suiteTitle: false
             }
-        ]
+        ],
+        ['html', { outputFolder: 'playwright-report', open: 'never' }]
     ],
 
     projects: [
